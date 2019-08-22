20b1b5612c71aec22b749a5f85dbfefa320c8316
Miguel Gonzalez <magonzalez@novasolutionsystems.com>
2019-08-22 17:00:16 -0500

    Updates

---
+++ precache-manifest.3e62e1aa436f8d63f2fa68b742e3a7e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76247f2a2c0a6054e069ca01436a8ec4",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "6a8f0d445f3a0044405d",
+    "url": "/test/static/css/main.884b0e11.chunk.css"
+  },
+  {
+    "revision": "21713b0729abfedc97fa",
+    "url": "/test/static/js/2.bb26c3fc.chunk.js"
+  },
+  {
+    "revision": "6a8f0d445f3a0044405d",
+    "url": "/test/static/js/main.d1927423.chunk.js"
+  },
+  {
+    "revision": "2160d828785e96a67ccd",
+    "url": "/test/static/js/runtime~main.eded8796.js"
+  },
+  {
+    "revision": "889a3fd45b9c077db2c586a4afb2ab31",
+    "url": "/test/static/media/default.889a3fd4.jpg"
+  }
+]);
